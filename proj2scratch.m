fd0ed219b857031734fcb19772dee8c7589e2990
serizawalynn <47999341+serizawalynn@users.noreply.github.com>
2021-12-08 22:07:05 -0500

    THIS WORKS

--- proj2scratch.m
+++ proj2scratch.m
@@ -1,8 +1,8 @@
-L = 595; % length of road, hard-coded
+L = 4550; % length of road, hard-coded
 % Set # of cars
-N = 10;
+N = 5;
 
-xc = -10*rand(N, 1); % sets up cars in random locations around the road
+xc = 200*rand(N, 1); % sets up cars in random locations around the road
 xc = sort(xc);
 
 CarInFront = [2 : N 1]; % 
@@ -11,8 +11,8 @@ allds = [];
 allvs = [];
 
 d = zeros(N , 1); % distance at each timestep
-dt = 5;
-Tf = 10800;
+dt = 0.5;
+Tf = 5000; %Parade took about three hours (10800 sec)
 stopcount = Tf/dt;
 
 %Intersections
@@ -23,7 +23,7 @@ vels = zeros(N,1); % velocities of the cars
 % Define dmin, dmax, vmax for case 2: dmax > L/N > dmin
 dmin = 0.9*(10/N); % hard-code this 
 dmax = 5*(10/N); % hard-code this also
-vmax = 0.1; % hard-code this also
+vmax = 2; % hard-code this also
 
 % Test plot of v
 dtest = linspace(0,2*dmax,100);
@@ -33,67 +33,78 @@ for iD=1:length(dtest)
     vtest(iD)=v(dtest(iD),dmin,dmax,vmax);
 end
 
+
+%%Code for movie/visuals
+my_figure=figure(1);
+vidya=VideoWriter("awesome.mp4");
+
+%video fps
+framerate_we_want=60;
+duration_we_want=10;
+framerate=framerate_we_want*duration_we_want;
+vidya.FrameRate=framerate;
+open(vidya)
+
 for iT=1:stopcount
+    iT
     % Calculate velocity of each car (fill in the array vels)
     for iCar=1:N
         % Update the car positions
         d(iCar) = xc(CarInFront(iCar)) - xc(iCar);
         
          % handle the case for the final car
-%         if (d(iCar) < 0)
-%             d(iCar) = d(iCar) + L;
-%         end
+        if (d(iCar) < 0)
+            d(iCar) = d(iCar) + L;
+        end
         
         % Update the car velocities
         for i=1:length(Intsct)
-            if (xc(iCar)>(Intsct(i)-10)) || (xc(iCar)<Intsct(i)+10)
+            if abs(Intsct(i)-xc(iCar))<5
+            %if (xc(iCar)>(Intsct(i)-5)) & (xc(iCar)<(Intsct(i)+5))
                 vels(iCar)=min(vels(iCar),1);
+                break %we're breaking because we only need to find the one intersection the car is at
             else
-                continue
+                vels(iCar) = v(d(iCar), dmin, dmax, vmax);
             end
         end
-        vels(iCar) = v(d(iCar), dmin, dmax, vmax);
+        
     end 
     
     % new position = old position + time*(distance/time) (in array-style)
     xc = xc + dt*vels;
     
     % Plot them in the route
-    A1 = [0,0];
-    B1 = [0, -200];
-
-    A2 = [80, 80];
-    B2 = [-200, -490];
-
-    A3 = [0, 80];
-    B3 = [-200, -200];
-
-    A4 = [55, 80];
-    B4 = [-490, -490];
-
-    plot(A1,B1)
+    plot([-10,4550],[0 0])
     hold on
-    plot(A2,B2)
-    plot(A3, B3)
-    plot(A4, B4)
+    plot(Intsct,0,'*')
     ylim([-600, 200])
-    axis([-10 10 -10 10])
+    
     
     %Plot vehicles
     
-    plot(xc,0,'.',MarkerSize=50)
-    hold on
+    plot(xc,0,'.',MarkerSize=25)
+    
     
     %Visuals
-  
+    axis([-10 4550 -10 10])
     drawnow
     hold off
     % Keep track of distance between car and that in front
     allds = [allds d];
     allvs = [allvs vels];
+    %xc
+    %vels
     
+    %Code for visuals
+    Mv=getframe(gcf);
+    writeVideo(vidya,Mv)
 end
+
+close(vidya)
+
+allvs_figure=figure(2);
 plot(allvs(1,:))
+drawnow
 
 function vv = v(d,dmin,dmax,vmax)
     if (d < dmin)
